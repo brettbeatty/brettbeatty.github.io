6c426127eb50d80c52c86a73b97b77f37cf6eb07
Brett Beatty <brettbeatty@gmail.com>
2024-03-20 18:38:31 -0600

    Touch up details
    - Remove last name from title
    - Add picture
    - Standardize page filenames
    - Get rid of home and show latest page

--- mix.exs
+++ mix.exs
@@ -4,23 +4,12 @@ defmodule MyProject do
   def project do
     [
       app: :my_app,
-      version: "2024.3.19",
+      version: "2024.3.20",
       elixir: "~> 1.16",
       deps: deps(),
       # docs
-      name: "Brett Beatty's Dev Blog",
-      docs: [
-        main: "home",
-        extras:
-          ["README.md": [filename: "home", title: "Home"]] ++
-            Path.wildcard("pages/**/*.{md,livemd}"),
-        groups_for_extras: [
-          Experiments: ~r(/experiments/),
-          Legacy: ~r(/legacy/)
-        ],
-        api_reference: false,
-        formatters: ["html"]
-      ]
+      name: "Brett's Dev Blog",
+      docs: docs()
     ]
   end
 
@@ -28,5 +17,32 @@ defmodule MyProject do
     [
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
     ]
+  end
+
+  defp docs do
+    pages =
+      "pages/**/*.{md,livemd}"
+      |> Path.wildcard()
+      |> Enum.sort_by(&Path.basename/1, :desc)
+
+    [
+      main: latest(pages),
+      extras: pages,
+      groups_for_extras: [
+        Experiments: ~r(/experiments/),
+        Legacy: ~r(/legacy/)
+      ],
+      api_reference: false,
+      formatters: ["html"],
+      # assets: "assets",
+      logo: "assets/logo.jpg"
+    ]
+  end
+
+  defp latest(pages) do
+    pages
+    |> hd()
+    |> Path.basename()
+    |> Path.rootname()
   end
 end
